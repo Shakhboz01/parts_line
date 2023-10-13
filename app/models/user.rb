5d6caa1f228eae902e6e5d8fd07aa8c1a9be893f
Shakhboz <shfazliddinov1940@gmail.com>
2023-10-13 08:39:52 +0500

    initialized project

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :participations
-  enum role: %i[админ менеджер другой]
+  enum role: %i[бухгалтер менеджер работник другой]
   validates :name, uniqueness: true
 
   def self.devise_parameter_sanitizer
