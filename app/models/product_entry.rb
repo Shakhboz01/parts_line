1324bcd74662c841008595ab430a2c4d42e8b6bc
Shakhboz <shfazliddinov1940@gmail.com>
2023-10-21 09:49:22 +0500

    added ransack and some naming fix

--- app/models/product_entry.rb
+++ app/models/product_entry.rb
@@ -8,12 +8,12 @@ class ProductEntry < ApplicationRecord
   validates_presence_of :provider, unless: -> { local_entry }
   validates_presence_of :buy_price, unless: -> { local_entry }
 
-  validate :amount_greater_tjan_amount_sold
+  validate :amount_greater_than_amount_sold
   before_commit :set_total_price
 
   private
 
-  def amount_greater_tjan_amount_sold
+  def amount_greater_than_amount_sold
     return errors.add(:base, 'amount is greater than amount sold') if amount < amount_sold
   end
 
