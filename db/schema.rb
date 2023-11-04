c02000b77c0883c4eb033666d1091a38ee166972
Shakhboz <shfazliddinov1940@gmail.com>
2023-11-04 09:31:33 +0500

    added weight to buyer and provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_115428) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_042828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_115428) do
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "weight", default: 0
   end
 
   create_table "combination_of_local_products", force: :cascade do |t|
@@ -135,6 +136,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_115428) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "weight", default: 0
   end
 
   create_table "salaries", force: :cascade do |t|
