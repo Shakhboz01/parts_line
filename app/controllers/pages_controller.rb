ca6dffc46b287f48880871172bf6555583d48a79
Shakhboz <shfazliddinov1940@gmail.com>
2023-11-20 16:28:29 +0500

    added main-page header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,28 @@
 class PagesController < ApplicationController
   def main_page
+    @buyers = Buyer.all.select { |buyer| buyer.calculate_debt > 0 }
+    @providers = Provider.all.select { |provider| provider.calculate_debt > 0 }
+    @delivery_from_counterparties = DeliveryFromCounterparty.unpaid.order(created_at: :desc)
+    @expenditures = Expenditure.unpaid.order(created_at: :desc)
+    @sales = Sale.unpaid.order(created_at: :desc)
+    @sale_from_local_services = SaleFromLocalService.unpaid.order(created_at: :desc)
+    @sale_from_services = SaleFromService.unpaid.order(created_at: :desc)
+
+    unless params.dig(:q, :created_at_end_of_day_lteq)
+      params[:q] ||= {}
+      params[:q][:created_at_end_of_day_lteq] = DateTime.current.end_of_day
+    end
+    unless params.dig(:q, :created_at_gteq)
+      params[:q] ||= {}
+      params[:q][:created_at_gteq] = DateTime.current.beginning_of_day
+    end
+    @q = TransactionHistory.ransack(params[:q])
+    @total_income = @q.result.where(expenditure_id: nil)
+      .where(delivery_from_counterparty_id: nil).sum(:price)
+
+    @total_expenditure = @q.result.where(sale_id: nil)
+      .where(sale_from_service_id: nil)
+      .where(sale_from_local_service_id: nil).sum(:price)
   end
 
   def define_sale_destination
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -5,4 +5,10 @@ class Provider < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
   scope :active, -> { where(:active => true) }
+
+  def calculate_debt
+    total_price = delivery_from_counterparties.sum(:total_price)
+    total_paid = delivery_from_counterparties.sum(:total_paid)
+    total_price - total_paid
+  end
 end
