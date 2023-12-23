b5bc44f59bf71518fdfaaa62edff669f4ccb0fc6
Shakhboz <shfazliddinov1940@gmail.com>
2023-12-23 22:41:35 +0500

    added chat_id field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_23_070117) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_23_174018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -345,6 +345,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_23_070117) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "telegram_chat_id"
+    t.string "string"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
