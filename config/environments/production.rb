24b9da8d743a55b3029ee0474e23355f6f787e9a
Shakhboz <shfazliddinov1940@gmail.com>
2023-12-25 22:34:53 +0500

    minor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.assets.enabled = false
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
